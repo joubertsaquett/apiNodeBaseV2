b32fe26202e3eeeea51cf141eb570b45641fdf08
Joubert Saquett <joubertsaquett@gmail.com>
2022-02-06 21:45:47 -0300

    feat(Security): Add cors and helmet

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
-const mailer = require('../../modules/mailer');
 
 const authConfig = require('../../config/auth');
 const User = require('../models/user');
